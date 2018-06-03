531f40727702348f3d42ac4e0f0e6e0c629b01b0
= <marcel.piet.00@gmail.com>
2018-06-04 00:49:28 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-import threading
 import pulsectl
 import curses
 import itertools
@@ -16,62 +15,12 @@ pulse = pulsectl.Pulse('pulse-volume-control')
 
 class sink:
     def __init__(self,sink_index):
-        self.max_index = len(pulse.sink_list())
-
         self.sink_index = sink_index
         self.sink_obj = pulse.sink_list()[sink_index]
         self.current_vol = pulse.volume_get_all_chans(self.sink_obj)
 
-    def change_vol(self,shift_step):
-        changed_vol = self.current_vol + shift_step
-        pulse.volume_set_all_chans(self.sink_obj,changed_vol)
-        
-
-def main():
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.curs_set(0)
-    stdscr.keypad(1)
-
-    index_array = []
-    for i in range(10):
-        try:
-            pulse.sink_list()[i]
-            index_array.append(i)
-        except IndexError:
-            break
-
-    index_iter = itertools.cycle(index_array)
-    current_index = next(index_iter)
-
-    print_elements(stdscr,current_index)
-    #screen.nodelay(1)
-    while True:
-        key = stdscr.getch()
-
-        if key == ord('q'):
-            break
-        elif (key == ord('a')) or (key == curses.KEY_LEFT):
-            try:
-                sink(current_index).change_vol(-0.01)
-                print_elements(stdscr,current_index)
-            except (pulsectl.pulsectl.PulseOperationInvalid):
-                pulse.volume_set_all_chans(sink(current_index).sink_obj,0)
-
-        elif key == ord('d') or (key == curses.KEY_RIGHT):
-            sink(current_index).change_vol(0.01)
-            print_elements(stdscr,current_index)
-
-        elif key == ord('w') or (key == curses.KEY_UP):
-            current_index = next(index_iter)
-            print_elements(stdscr,current_index)
-
-        elif key == ord('s') or (key == curses.KEY_DOWN):
-            current_index = next(index_iter)
-            print_elements(stdscr,current_index)
-        
-
-    curses.endwin()
+    def change_vol(self,step):
+        pulse.volume_change_all_chans(self.sink_obj,step)
 
 
 def print_elements(stdscr,sink_index):
@@ -91,12 +40,13 @@ def print_elements(stdscr,sink_index):
 
     #Show volume level as status bar
     if args.b == "true":
+
         volume = sink(sink_index).current_vol
         max_height,max_width = stdscr.getmaxyx() 
 
-        for i in range(max_width): #First layer "------------"
-            stdscr.addch(3,i,'—', curses.A_DIM) 
-            stdscr.addch(4,i,'—', curses.A_DIM)
+        for i in range(max_width): 
+            stdscr.addch(3,i,'‾', curses.A_DIM) 
+            stdscr.addch(4,i,'_', curses.A_DIM)
 
         stdscr.addch(3,0,'▕', curses.A_DIM)
         stdscr.addch(4,0,'▕', curses.A_DIM)
@@ -107,14 +57,65 @@ def print_elements(stdscr,sink_index):
         bar_width = int(bar_width)
 
         if volume >= 1:
-            bar_width = max_width-1 # If volume is 100% or higher arrow doesn't go behind '|'
+            bar_width = max_width-1 # If volume is 100% or higher bar doesn't go behind '|'
 
         try:
             for i in range(1,bar_width):
-                stdscr.addch(3,i,'▄', curses.A_DIM)
-                stdscr.addch(4,i,'▀', curses.A_DIM)
+                stdscr.addch(3,i,'█', curses.A_DIM)
+                stdscr.addch(4,i,'█', curses.A_DIM)
         except (curses.error):
             pass
+
+
+def main():
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.curs_set(0)
+    stdscr.keypad(1)
+
+    index_array = []
+    for i in range(10):
+        try:
+            pulse.sink_list()[i]
+            index_array.append(i)
+        except IndexError:
+            break
+
+    index_iter = itertools.cycle(index_array)
+    current_index = next(index_iter)
+
+    print_elements(stdscr,current_index)
+    
+    try:
+        while True:
+            key = stdscr.getch()
+
+            if key == ord('q'):
+                break
+                
+            elif (key == ord('a')) or (key == curses.KEY_LEFT):
+                try:
+                    sink(current_index).change_vol(-0.01)
+                    print_elements(stdscr,current_index)
+                except (pulsectl.pulsectl.PulseOperationInvalid):
+                    pulse.volume_set_all_chans(sink(current_index).sink_obj,0)
+
+            elif key == ord('d') or (key == curses.KEY_RIGHT):
+                sink(current_index).change_vol(0.01)
+                print_elements(stdscr,current_index)
+
+            elif key == ord('w') or (key == curses.KEY_UP):
+                current_index = next(index_iter)
+                print_elements(stdscr,current_index)
+
+            elif key == ord('s') or (key == curses.KEY_DOWN):
+                current_index = next(index_iter)
+                print_elements(stdscr,current_index)
+    
+    except KeyboardInterrupt:
+        pass
+        
+    curses.endwin()
 
 
 if __name__ == "__main__":
